247ef1622dcbff4dd720e70b3f3a352c13511469
giorgio aurispa <jaws24@hotmail.it>
2017-11-29 16:31:15 +0100

    download of images added

--- link_finder.py
+++ link_finder.py
@@ -1,5 +1,7 @@
 from html.parser import HTMLParser
 from urllib import parse
+from urllib.request import urlretrieve
+from domain import *
 
 
 class LinkFinder(HTMLParser):
@@ -17,6 +19,17 @@ class LinkFinder(HTMLParser):
                 if attribute == 'href':
                     url = parse.urljoin(self.base_url, value)
                     self.links.add(url)
+        elif tag == 'img':
+            for (attribute, value) in attrs:
+                if attribute == 'src':
+                    print('image found!: ' + value)
+                    filename = value.split('/')[-1]
+                    try:
+                        urlretrieve(value, 'youtube/' + filename)
+                    except Exception as e:
+                        print(e)
+
+
 
     def page_links(self):
         return self.links
